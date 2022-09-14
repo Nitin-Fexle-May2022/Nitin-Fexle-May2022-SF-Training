e0b778744a503a790295f541f27492f17fa2d419
nitin70266 <rajput.nitin70266@gmail.com>
2022-09-14 11:53:43 +0530

    Revert "Lwc-Assignment-3 First commit"
    This reverts commit f9b480236d018c0bb828a6482c0aadbf8fbedc99.

--- src/lwc/childWebComponent/childWebComponent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track,api } from "lwc";
-
-export default class ChildWebComponent extends LightningElement {
-  @track value=100; 
-  @api handleValueChange() {
-    this.value=200;
-  }
-}
--- src/lwc/parentLwc/parentLwc.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track } from "lwc";
-
-export default class ParentLwc extends LightningElement {
-  @track progressVal = 0;
-  hanldeProgressValueChange(event) {
-    this.progressVal = event.detail;
-  }
-}
